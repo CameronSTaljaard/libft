c580f448e7619d9f1da4ecb715fed85fcd9507b4
CameronSTaljaard <CameronSTaljaard@gmail.com>
2019-05-20 15:10:58 +0200

    Commiting experimental changes to ft_putchar, attempting to add unicode support.

--- ft_putchar.c
+++ ft_putchar.c
@@ -2,5 +2,15 @@
 
 void	ft_putchar(int c)
 {
-	write(1, &c, 1);
+	int test;
+
+	test = ((unsigned char *)(&c))[0];
+	if (0xC2 <= test && test <= 0xDF)
+            write(1, &c, 2);
+		else if (0xE0 <= test && test <= 0xEF)
+            write(1, &c, 3);
+        else if (0xF0 <= test && test <= 0xF4)
+            write(1, &c, 4);
+        else
+    		write(1, &c, 1);
 }
