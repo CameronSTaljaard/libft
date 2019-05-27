939dec12f1efdef9c7598684b886f286a66d7b7e
Ameen HARTLEY <ctaljaar@c3r7s6.wethinkcode.co.za>
2019-05-27 13:42:17 +0200

    Commiting a fix to ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -15,9 +15,9 @@
 char	*ft_strchr(const char *str, int c)
 {
 	size_t	i;
-
+	
 	i = 0;
-	while (str[i] != (char)c)
+	while (str[i] != (char)c && str[i])
 		i++;
 	if (str[i] == (char)c)
 		return ((char *)&str[i]);
